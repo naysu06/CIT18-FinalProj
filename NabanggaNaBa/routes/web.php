8383a5138d3c651fc6ca5c2b3fa6a1f1c7cbd33f
naysu06 <113022553+naysu06@users.noreply.github.com>
2025-03-17 04:24:57 +0000

    inital routes

--- NabanggaNaBa/routes/web.php
+++ NabanggaNaBa/routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/app', function () {
+    return view('app');
+});
+
+Route::get('/AdminLogin', function () {
+    return view('AdminLogin');
+});
+
+Route::resource('admin', AdminController::class);
+
+Route::resource('main', MainController::class);
