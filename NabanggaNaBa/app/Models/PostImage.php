8b442b87122e5c0ccd5de9bf9c3dc2878d00d8d1
naysu06 <franzdominicc@gmail.com>
2025-04-09 22:04:37 +0800

    Fixed images in frontend

---
+++ NabanggaNaBa/app/Models/PostImage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostImage extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['vehicle_id', 'image_path'];
+
+    public function vehicle()
+    {
+        return $this->belongsTo(Post::class, 'vehicle_id');
+    }
+}
